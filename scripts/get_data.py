39fb1781aa76cb1822ca1ef3bb9cbee4804434f8
Jordan Palmer <jordan.palmer19@gmail.com>
2018-06-07 18:56:51 -0400

    Get data no longer requires aws credentials

--- scripts/get_data.py
+++ scripts/get_data.py
@@ -5,26 +5,18 @@ import yaml
 import os
 import errno
 from pathlib import Path 
+import requests
 
 def get_data():
 	# Every file is ogranized like 
 	# https://s3.amazonaws.com/nyc-tlc/trip+data/[color]_tripdata_[year]-[month].csv
 	# where color can be fhv, green or yellow and the year goes back to 2009
-	# s3 = boto3.resource("s3")
-	now = datetime.datetime.now()
-	colors = ["fhv", "green", "yellow"]
-	# Creates the yaml connection
-	with open('s3.yaml', 'r') as f:
-		config = yaml.load(f)
-	connection = s3.S3Connection(**config['s3'])
-	# Creates the storage object
-	storage = s3.Storage(connection)
-	print(str(storage)+"\n \n \n \n") 
+
 	# Checks to see if the file is containing Zones exists. If it doesn't exist 
 	# the file is downloaded or else the current one is kept. Change funcionality
-	# to replace old file?
+	# to  replace old file?
 	try:
-		dataFile = Path("taxi+_zone_lookup.csv")
+		dataFile = Path("data/taxi+_zone_lookup.csv")
 		absPath = dataFile.resolve()	
 	except OSError as e:
 		if e.errno == errno.ENOENT:
@@ -32,24 +24,25 @@ def get_data():
 	else:
 		pass
 	#	Every combination of color, year, and month a file could contain
+	now = datetime.datetime.now()
 	for year in range(2009, now.year):
+		colors = ["fhv", "green", "yellow"]
 		for month in ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]:
 			for color in colors:
 				# Key is created
-				key = "trip+data/"+color+"_tripdata_"+str(year)+"-"+month+".csv"
-#											  #
-# This call of exist is just to see the error #
-# 											  #
-				exists = storage.exists(key)
-				# Checks if the key works to download a file
+				key = color+"_tripdata_"+str(year)+"-"+month+".csv"
+				# Checks if file has been downloaded
 				try:
-					exists = storage.exists(key)
-					print('A key exists')
-				# The key doesn't work and it moves to the next key
-				except:
-					continue
-				# The key does work and is downloaded
-				wget.download("https://s3.amazonaws.com/nyc-tlc/"+key, out=("data--"+color+"--"+str(year)+"--"+month))	
+					dataFile = Path("data/"+key)
+					absPath = dataFile.resolve()
+					print(key+" already exists")
+				# The file is downloaded
+				except OSError as e:
+					if e.errno == errno.ENOENT:
+						# Makes sure there is a csv file for the address
+						if requests.head("https://s3.amazonaws.com/nyc-tlc/trip+data/"+key).status_code == 200:
+							print("this worked  "+key)	
+							wget.download("https://s3.amazonaws.com/nyc-tlc/trip+data/"+key, out=key)	
 
 			
 get_data()
